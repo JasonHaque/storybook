041c0ed1d57855f2b84d60fd8350cff7525359ce
JasonHaque <zahinjason220434@gmail.com>
2020-04-01 15:46:29 +0600

    ib action and outlets added

--- StoryAppiOS/View Controllers/LogInViewController.swift
+++ StoryAppiOS/View Controllers/LogInViewController.swift
@@ -9,22 +9,19 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    @IBOutlet weak var EmailAddress: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var LogInErrorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        LogInErrorLabel.text=""
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func LogInTapped(_ sender: Any) {
     }
-    */
+    
+   
 
 }
