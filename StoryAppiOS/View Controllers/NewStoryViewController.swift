55331ec877728ba65d48af45725e8895c19bee6f
JasonHaque <zahinjason220434@gmail.com>
2020-04-03 16:59:55 +0600

    ib listeners added

--- StoryAppiOS/View Controllers/NewStoryViewController.swift
+++ StoryAppiOS/View Controllers/NewStoryViewController.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class NewStoryViewController: UIViewController {
 
+    @IBOutlet weak var StoryNameField: UITextField!
+    @IBOutlet weak var StoryContent: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func SaveButtonTapped(_ sender: Any) {
     }
-    */
-
 }
