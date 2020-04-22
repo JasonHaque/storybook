49532f54704c3ee6b967018e2c925c03d73bfe52
JasonHaque <zahinjason220434@gmail.com>
2020-04-22 09:40:50 +0600

    auth state checker added

--- StoryAppiOS/View Controllers/ViewController.swift
+++ StoryAppiOS/View Controllers/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkCurrentUser()
         
+    }
+    
+    func checkCurrentUser(){
+       Auth.auth().addStateDidChangeListener { (auth, user) in
+            if user != nil {
+                self.transitionHome()
+            } else {
+                 
+            }
+        }
+    }
+    
+    func transitionHome(){
+        let homeView = storyboard?.instantiateViewController(identifier: "HomeView") as? HomeViewController
+        view.window?.rootViewController = homeView
+        view.window?.makeKeyAndVisible()
     }
 
 
