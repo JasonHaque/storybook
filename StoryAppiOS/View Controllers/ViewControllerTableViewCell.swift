e69487aff90fae79df0c114d616fa72f5de48d37
JasonHaque <zahinjason220434@gmail.com>
2020-04-23 04:07:55 +0600

    tableview cell added

---
+++ StoryAppiOS/View Controllers/ViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTableViewCell.swift
+//  StoryAppiOS
+//
+//  Created by Sanviraj Zahin Haque on 23/4/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
